935c726ced063c3ca7f613783629bd7ac9334997
Empty <empty@MacBook-Air-Empty.local>
2025-12-16 22:59:04 +0500

    Init

---
+++ Lecture2/index.js
@@ -0,0 +1,27 @@
+//const serverPort = 3000;
+//serverPort = 2000;
+
+//const won't change but with let there is room to change information in variable
+//let status = "Ожидание";
+//console.log(status) 
+//status = "Оплата прошла успешно"
+//console.log(status)
+
+//concst strtbs = 14 + '18';
+//will be printed 1418
+
+const age = "18";
+
+//if (age >= 18) {
+//    console.log("Доступ разрешен")
+//} else{
+//    console.log("иди домой")
+//}
+
+//difference between == and ===
+
+if (age == 18) {
+    console.log("Доступ разрешен")
+} else{
+    console.log("иди домой")
+}
